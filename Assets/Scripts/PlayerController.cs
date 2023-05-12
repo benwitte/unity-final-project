c96270edfdd6a80183a2904adbe624694d557e66
Shloon <shlomo.shmouely@gmail.com>
2023-05-12 21:25:59 +0300

    feat(player-controller): add support for turning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,8 @@ namespace InfiRun
 
         CharacterController characterController;
         float yspeed;
+        bool isRotating;
+
 
         public float jumpSpeed = 15;
         public float moveSpeed = 5f;
@@ -22,6 +24,7 @@ namespace InfiRun
         public void Update()
         {
             var movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            movement = transform.rotation * movement;
 
             if (characterController.isGrounded)
             {
@@ -41,6 +44,30 @@ namespace InfiRun
             velocity.y = yspeed;
 
             characterController.Move(velocity * Time.deltaTime);
+        }
+
+        IEnumerator TurnCoroutine(bool turnRight)
+        {
+            const float lerpDuration = 0.5f;
+            isRotating = true;
+            float timeElapsed = 0;
+            Quaternion startRotation = transform.rotation;
+            Quaternion targetRotation = transform.rotation * Quaternion.Euler(0, turnRight ? 90 : -90, 0);
+
+            while (timeElapsed < lerpDuration)
+            {
+                transform.rotation = Quaternion.Slerp(startRotation, targetRotation, timeElapsed / lerpDuration);
+                timeElapsed += Time.deltaTime;
+                yield return null;
+            }
+            transform.rotation = targetRotation;
+            isRotating = false;
+        }
+
+        public void Turn(bool turnRight)
+        {
+            if (isRotating) return;
+            StartCoroutine(TurnCoroutine(turnRight));
         }
     }
 }
