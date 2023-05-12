4ad51c19b459f24addd5fd52e11c18737cc3a9fd
Shloon <shlomo.shmouely@gmail.com>
2023-05-12 22:24:00 +0300

    chore: add score view

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,5 +7,14 @@ namespace InfiRun
         public CameraController cameraController;
         public PlayerController playerController;
         public WorldController worldController;
+        public TMPro.TextMeshProUGUI scoreLabel;
+
+        int score = 0;
+
+        void Update()
+        {
+            score++;
+            scoreLabel.text = score.ToString();
+        }
     }
 }
